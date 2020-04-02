39076fe2ae6471803cb797cb2dce3a17ef086a99
Andrzej Markowski <andrzej@kreatzje.pl>
2020-04-02 21:09:30 +0200

    modules

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
-export function castVariable(variable) {
-  let isJson = false;
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = global || self, factory(global.acorn = {}));
+}(this, function (exports) {
+  function castVariable(variable) {
+    let isJson = false;
 
-  try {
-    isJson = typeof JSON.parse(variable) === 'object';
-  } catch (e) {
-    // eslint-disable-line
+    try {
+      isJson = typeof JSON.parse(variable) === 'object';
+    } catch (e) {
+      // eslint-disable-line
+    }
+
+    if (
+      typeof variable === 'string'
+      && (variable.match(/^(\d)+$/)
+        || variable.match(/^(\d)+\.(\d)+$/)
+        || variable.match(/^true|false$/)
+        || isJson)
+    ) {
+      return JSON.parse(variable);
+    }
+
+    return variable;
   }
 
-  if (
-    typeof variable === 'string'
-    && (variable.match(/^(\d)+$/)
-      || variable.match(/^(\d)+\.(\d)+$/)
-      || variable.match(/^true|false$/)
-      || isJson)
-  ) {
-    return JSON.parse(variable);
-  }
+  exports.castVariable = castVariable;
 
-  return variable;
-}
+  Object.defineProperty(exports, '__esModule', { value: true });
+}));
