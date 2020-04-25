da7d3d6fcf3cac08becd1a6ed90e263693bc42a2
Andrzej Markowski <andrzej@kreatzje.pl>
2020-04-25 07:44:46 +0200

    ES5

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@
        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
     };
 
-    return `${S4()}${S4()}${S4()}${S4()}`;
+    return S4() + S4() + S4() + S4();
   }
 
   exports.castVariable = castVariable;
