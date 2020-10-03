3da5b2dccbfaaf51c14f8331cf2f44cbe0fadda2
Andrzej Markowski <andrzej@kreatzje.pl>
2020-10-03 21:41:57 +0200

    export vue

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import HiwayButton from '../vue/HiwayButton';
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -35,6 +37,7 @@
 
   exports.castVariable = castVariable;
   exports.getUID = getUID;
+  exports.HiwayButton = HiwayButton;
 
   Object.defineProperty(exports, '__esModule', { value: true });
 }));
