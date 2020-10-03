8d75275146cd19a94033813f8fac82c5fb230182
Andrzej Markowski <andrzej@kreatzje.pl>
2020-10-03 21:08:52 +0200

    hiway button

---
+++ vue/HiwayButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    :class="classes"
+    :type="type"
+    v-on="$listeners"
+  >
+    <slot/>
+    <span
+      v-if="isLoading"
+      class="loader"
+    />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      required: false,
+      type: String,
+    },
+    isLoading: {
+      required: false,
+      type: Boolean,
+    },
+    size: {
+      required: false,
+      type: String,
+    },
+    type: {
+      required: false,
+      type: String,
+      default: 'button',
+    },
+  },
+
+  computed: {
+    classes() {
+      const classes = ['btn'];
+
+      if (this.color) {
+        classes.push(`btn-${this.color}`);
+      } else {
+        classes.push('btn-default');
+      }
+
+      if (this.isLoading) {
+        classes.push('btn-loading');
+      }
+
+      if (this.size) {
+        classes.push(`btn-${this.size}`);
+      }
+
+      return classes;
+    },
+  },
+};
+</script>
