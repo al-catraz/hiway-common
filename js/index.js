a0af9317318f898179886a5fb41c35eda096afe8
Andrzej Markowski <andrzej@kreatzje.pl>
2020-10-18 15:34:20 +0200

    better export

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 /* global define */
+import castVariable from './castVariable';
+import getUID from './getUID';
+
 (function (global, factory) {
   if (typeof exports === 'object' && typeof module !== 'undefined') {
     factory(exports);
@@ -10,39 +13,9 @@
     factory(global.acorn = {});
   }
 }(this, function (exports) { // eslint-disable-line prefer-arrow-callback
-  function castVariable(variable) {
-    let isJson = false;
-
-    try {
-      isJson = typeof JSON.parse(variable) === 'object';
-    } catch (e) {
-      // eslint-disable-line
-    }
-
-    if (
-      typeof variable === 'string'
-      && (variable.match(/^(\d)+$/)
-        || variable.match(/^(\d)+\.(\d)+$/)
-        || variable.match(/^true|false$/)
-        || isJson)
-    ) {
-      return JSON.parse(variable);
-    }
-
-    return variable;
-  }
-
-  function getUID() {
-    function S4() {
-      // eslint-disable-next-line no-bitwise
-      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-    }
-
-    return S4() + S4() + S4() + S4();
-  }
-
-  exports.castVariable = castVariable;
-  exports.getUID = getUID;
-
+  Object.assign(exports, {
+    castVariable,
+    getUID,
+  });
   Object.defineProperty(exports, '__esModule', { value: true });
 }));
