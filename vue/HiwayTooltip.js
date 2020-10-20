5a72f8aed001317c12f3b94e7d5e09647140b72b
Andrzej Markowski <andrzej@kreatzje.pl>
2020-10-20 21:52:51 +0200

    tooltip style

--- vue/HiwayTooltip.js
+++ vue/HiwayTooltip.js
@@ -1,19 +1,17 @@
 import { VTooltip } from 'v-tooltip';
 
 const { options } = VTooltip;
-const arrowClass = 'arrow';
 const innerClass = 'tooltip-inner';
 
 VTooltip.options = {
   ...options,
-  defaultArrowSelector: `.${arrowClass}`,
   defaultBaseClass: 'tooltip',
   defaultClass: '',
   defaultContainer: 'body',
   defaultHtml: true,
   defaultInnerSelector: `.${innerClass}`,
   defaultPlacement: 'top',
-  defaultTemplate: `<div class="tooltip show" role="tooltip"><div class="${arrowClass}"></div><div class="${innerClass}"></div></div>`,
+  defaultTemplate: `<div class="tooltip in" role="tooltip"><div class="${innerClass}"></div></div>`,
   defaultTrigger: 'hover focus',
   defaultWrapperClass: '',
 };
