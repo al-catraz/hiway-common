231725063793626cbcf40bbc9a369c485cecbb57
Andrzej Markowski <andrzej@kreatzje.pl>
2020-10-27 21:50:00 +0100

    modal

--- vue/index.js
+++ vue/index.js
@@ -1,11 +1,13 @@
 import HiwayButton from './HiwayButton.vue';
 import HiwayDropdown from './HiwayDropdown.vue';
 import HiwayDropdownItem from './HiwayDropdownItem.vue';
+import HiwayModal from './HiwayModal.vue';
 import HiwayTooltip from './HiwayTooltip';
 
 export {
   HiwayButton,
   HiwayDropdown,
   HiwayDropdownItem,
+  HiwayModal,
   HiwayTooltip,
 };
