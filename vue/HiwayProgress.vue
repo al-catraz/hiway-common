13a3087a2b0343aa874212c62b09abab9f0a10f1
Andrzej Markowski <andrzej@kreatzje.pl>
2020-11-01 16:24:26 +0100

    progress

---
+++ vue/HiwayProgress.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="progress">
+    <div
+      class="progress-bar"
+      :class="{ [`bg-${color}`]: true }"
+      :style="`width: ${percent}%;`"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HiwayProgress',
+
+  props: {
+    color: {
+      required: false,
+      type: String,
+      default: 'green-04',
+    },
+    value: {
+      required: true,
+      type: Number,
+    },
+  },
+
+  computed: {
+    percent() {
+      return Math.round((this.value / 100) * 100);
+    },
+  },
+};
+</script>
--- vue/index.js
+++ vue/index.js
@@ -2,6 +2,7 @@ import HiwayButton from './HiwayButton.vue';
 import HiwayDropdown from './HiwayDropdown.vue';
 import HiwayDropdownItem from './HiwayDropdownItem.vue';
 import HiwayModal from './HiwayModal.vue';
+import HiwayProgress from './HiwayProgress.vue';
 import HiwayTooltip from './HiwayTooltip';
 
 export {
@@ -9,5 +10,6 @@ export {
   HiwayDropdown,
   HiwayDropdownItem,
   HiwayModal,
+  HiwayProgress,
   HiwayTooltip,
 };
